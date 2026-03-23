716340e20fdbf790e96477ae84865c320718da53
Mirazen <danya-galenko@yandex.ru>
2026-03-24 00:36:01 +0300

    feat: complete step 6 - migrate from SQLite to PostgreSQL

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -2,27 +2,27 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    dialect: 'sqlite',
-    storage: './database.sqlite',
+    dialect: 'postgres',
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT || 5432,
+    host: process.env.DATABASE_HOST,
   },
   production: {
     dialect: 'postgres',
     database: process.env.DATABASE_NAME,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
+    port: process.env.DATABASE_PORT || 5432,
     host: process.env.DATABASE_HOST,
   },
   test: {
-    dialect: 'sqlite',
-    storage: './database.test.sqlite',
+    dialect: 'postgres',
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT || 5432,
+    host: process.env.DATABASE_HOST,
   },
-  // test: {
-  //   dialect: 'postgres',
-  //   database: process.env.DATABASE_NAME,
-  //   username: process.env.DATABASE_USERNAME,
-  //   password: process.env.DATABASE_PASSWORD,
-  //   port: process.env.DATABASE_PORT,
-  //   host: process.env.DATABASE_HOST,
-  // },
 };
