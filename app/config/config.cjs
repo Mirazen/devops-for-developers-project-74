618d301078e70d9b92303afc6ad7ae9c83b97105
Mirazen <danya-galenko@yandex.ru>
2026-03-24 00:57:36 +0300

    chore: fix host-side setup and smart database config

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -1,28 +1,22 @@
 require('dotenv').config();
 
+const postgresConfig = {
+  dialect: 'postgres',
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT || 5432,
+};
+
+const sqliteConfig = (path) => ({
+  dialect: 'sqlite',
+  storage: path,
+});
+
 module.exports = {
-  development: {
-    dialect: 'postgres',
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT || 5432,
-    host: process.env.DATABASE_HOST,
-  },
-  production: {
-    dialect: 'postgres',
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT || 5432,
-    host: process.env.DATABASE_HOST,
-  },
-  test: {
-    dialect: 'postgres',
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT || 5432,
-    host: process.env.DATABASE_HOST,
-  },
+  // Если DATABASE_HOST есть, используем Postgres, иначе SQLite
+  development: process.env.DATABASE_HOST ? postgresConfig : sqliteConfig('./database.sqlite'),
+  test: process.env.DATABASE_HOST ? postgresConfig : sqliteConfig('./database.test.sqlite'),
+  production: postgresConfig,
 };
